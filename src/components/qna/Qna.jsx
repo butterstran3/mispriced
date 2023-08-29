41a8f6e43a8296347efd81eb5e4d0c05b605ff4d
Jimmy Tran <jimmitran3@gmail.com>
2023-08-29 15:28:08 +1000

    Complete qna component

---
+++ src/components/qna/Qna.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./qna.css";
+
+const Qna = () => {
+    return (
+        <div className="qna_container">
+            <div className="qna_content">
+                <h4 className="qna_title">The Mispriced Method</h4>
+                <div className="qna_section">
+                    <h1 className="qna_question">So how does it work?</h1>
+                    <p className="qna_answer">
+                        Our independent, quantitative approach to EV betting allows us to outsmart the bookies consistently. Boasting a proven track record and delivering a 30% turnover, our advanced algorithm alerts you to lucrative EV bets so you stay ahead in the game and consistently generate profit. 
+                        <br></br>
+                        <br></br>
+                        Unlike bookie affiliated tipping services, we focus on delivering +EV bets and educating our members to find their own +EV.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Qna;
