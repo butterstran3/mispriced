e3bff2b1d02f338d70d166d7927639baaff920f2
Jimmy Tran <jimmitran3@gmail.com>
2023-12-01 22:34:24 +1100

    Fix home title

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,8 @@ const Home = () => {
         <div className="home_container">
             <div className="home_content">
                 <div className="home_info">
-                    <h1>Bet faster, bet smarter</h1>
+                    <h1>Bet faster,</h1>
+                    <h1>bet smarter</h1>
                     <p>Mispriced helps you beat the bookies and maximise your chances of winning.</p>
                     <Link className="home_btn btn" to="https://discord.gg/VR3ncXNzG6">Try Mispriced now</Link>
                 </div>
